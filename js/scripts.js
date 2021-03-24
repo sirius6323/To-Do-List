83588bf58b119db8e4341155afd07b6aa02e639c
sirius6323 <sirius6323@gmail.com>
2021-03-23 17:22:47 -0700

    If applied, added JS to script file

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,70 @@
-// Variables to list and list item
-let list = $('#list');
-let li = $('<li></li>');
-let listInputValue = $('#input').val();
+function newItem() {
+	//javascript
+	//1. Adding a new item to the list of items:
+	let li = document.createElement('li');
+	let inputValue = document.getElementById('input').value;
+	let text = document.createTextNode(inputValue);
+	li.appendChild(text);
 
-// Appends list item to list
-list.append(li);
-li.append(listInputValue);
-listInputValue.append(li);
+	if (inputValue === '') {
+		alert('You must write something!');
+	} else {
+		let list = document.querySelector('#list');
+		list.appendChild(li);
+	}
 
-// Strikes out list item
-//li.addClass('strike');
+	//2. Crossing out an item from the list of items:
+	function crossOut() {
+		li.classList.toggle('strike');
+	}
+
+	li.addEventListener('dblclick', crossOut);
+
+	//3(i). Adding the delete button "X":
+	let crossOutButton = document.createElement('crossOutButton');
+	crossOutButton.appendChild(document.createTextNode('X'));
+	li.appendChild(crossOutButton);
+
+	crossOutButton.addEventListener('click', deleteListItem);
+	//3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+	function deleteListItem() {
+		li.classList.add('delete');
+	}
+	// 4. Reordering the items:
+	$('#list').sortable();
+}
+
+// If you get stuck, you can look below for the jQuery code. However, try yourself to convert the vanilla JS code provided to jQuery first.
+
+/*
+	// jQuery Code
+	//1. Adding a new item to the list:
+	
+		let li = $('<li></li>');
+		let inputValue = $('#input').val();
+		li.append(inputValue);
+	
+		if (inputValue === '') {
+			alert("You must write something!");
+		} else {
+			$('#list').append(li);
+		}
+	//2. Crossing an item out:
+		function crossOut() {
+			li.toggleClass("strike");
+		}
+	
+		li.on("dblclick", function crossOut() {
+			li.toggleClass("strike");
+		});
+	//3. Adding a delete button
+		let crossOutButton = $('<crossOutButton></crossOutButton>');
+		crossOutButton.append(document.createTextNode('X'));
+		li.append(crossOutButton);
+	
+	//   crossOutButton.on("click", deleteListItem);
+	//   function deleteListItem(){
+	// 		li.addClass("delete")
+	// 	}
+		 $('#list').sortable();
+	*/
